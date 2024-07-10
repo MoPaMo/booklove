f0981a4a4a12c893f2d0dba85d7adf926125bedb
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-10 20:01:16 +0200

    Initial Commit

---
+++ booklove/bookloveApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookloveApp.swift
+//  booklove
+//
+//  Created by Moritz on 10.07.24.
+//
+
+import SwiftUI
+
+@main
+struct bookloveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
