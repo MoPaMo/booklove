dcfb9a966f9ad58281a6e761f7ae3b243bde85c7
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-11 10:26:33 +0200

    work on reel section

--- booklove/Quotes.swift
+++ booklove/Quotes.swift
@@ -7,39 +7,74 @@ struct Quotes: View {
             Color(red: 1, green: 0.98, blue: 0.98)
                 .edgesIgnoringSafeArea(.all)
             
-            // Background Blur Elements
-            BlurredBackgroundElements()
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(width: 307.14682, height: 292.85287)
+                .background(
+                    Image("blur_base")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 307.14682, height: 292.85287)
+                        .clipped()
+                )
+                .cornerRadius(65)
+                .shadow(color: .white.opacity(0.25), radius: 3, x: -10, y: 4)
+                .blur(radius: 6)
+                .rotationEffect(Angle(degrees: -58.16))
+                .offset(x: -130, y: -300)
+            
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(width: 375.42, height: 341.27)
+                .background(
+                    Image("blur_hex")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                )
+                .cornerRadius(65)
+                .offset(x: 130, y: 300)
+                .rotationEffect(.degrees(-6.25))
+                .blur(radius: 6)
             
             VStack(spacing: 20) {
+                
                 // Header
                 Text("booklove.")
-                    .font(.custom("NewYork", size: 55))
-                    .fontWeight(.bold)
+                    .font(.system(size: 64, weight: .bold, design: .serif))
                     .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 20)
                 
                 // Main Content
                 HStack( spacing: 20) {
                     VStack{ //card
+                        Rectangle()
+                          .foregroundColor(.clear)
+                          .frame(width: 311, height: 512)
+                          .background(.white.opacity(0.1))
+                          .cornerRadius(37)
+                          .shadow(color: .black.opacity(0.2), radius: 4, x: 0, y: 4)
                         // Quote
-                        Text("You must allow me to tell you how ardently I love and admire you.")
-                            .font(.custom("NewYorkLarge", size: 40))
+                        Text("      You must allow me to tell you how ardently I love and admire you.")
+                            .font(.system(size: 40, weight: .regular, design: .serif))
                             .foregroundColor(.black)
                             .padding(.vertical)
                         
                         Text("-Mr. Darcy")
-                            .font(.system(size: 20, weight: .regular, design: .rounded))
+                            .font(.system(size: 20, weight: .light, design: .rounded))
                             .foregroundColor(.black)
                         
                         // Book Information
                         Text("Pride and Prejudice")
-                            .font(.custom("NewYork", size: 24))
-                            .foregroundColor(Color(#colorLiteral(red: 1, green: 0.18, blue: 0.33, alpha: 1)))
+                            .font(.system(size: 24, weight: .heavy, design: .serif))
+
+                            .foregroundColor(.red)
                         
                         Text("Jane Austen, 1813")
-                            .font(.custom("SFMono", size: 20))
+                            .font(.system(size: 20, weight: .light, design: .monospaced))
                             .foregroundColor(.black)
                         .kerning(-2)}.padding()
-                        .background(Color.white.opacity(0.2))
+                        .background(Color.white.opacity(0.1))
                         .cornerRadius(37)
                         .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 4)
                     
@@ -60,26 +95,6 @@ struct Quotes: View {
             }
             .padding(.top, 50)
             
-        }
-    }
-}
-
-struct BlurredBackgroundElements: View {
-    var body: some View {
-        ZStack {
-            Image("blur_base")
-                .resizable()
-                .frame(width: 307, height: 293)
-                .rotationEffect(Angle(degrees: -58))
-                .offset(x: -130, y: -300)
-                .blur(radius: 6)
-            
-            Image("blur_hex")
-                .resizable()
-                .frame(width: 375, height: 341)
-                .rotationEffect(Angle(degrees: -6))
-                .offset(x: 130, y: 300)
-                .blur(radius: 6)
         }
     }
 }
