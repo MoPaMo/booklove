cddae726bded33ddf091f16a81956303ed0bb7eb
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-11 08:02:15 +0200

    add tabs

---
+++ booklove/GeneralView.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralView.swift
+//  booklove
+//
+//  Created by Moritz on 11.07.24.
+//
+
+import SwiftUI
+
+struct GeneralView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeneralView()
+}
---
+++ booklove/Quotes.swift
@@ -0,0 +1,18 @@
+//
+//  Quotes.swift
+//  booklove
+//
+//  Created by Moritz on 11.07.24.
+//
+
+import SwiftUI
+
+struct Quotes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Quotes()
+}
