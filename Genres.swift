0548d4e882b2960243cc569cb5b9fb7a127ea46a
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-12 16:16:18 +0200

    create genres

---
+++ Genres.swift
@@ -0,0 +1,21 @@
+//
+//  Genres.swift
+//  booklove
+//
+//  Created by Moritz on 12.07.24.
+//
+
+import SwiftUI
+
+struct Genres: View {
+    var body: some View {
+        ZStack {
+            BackgroundBlurElement()
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+#Preview {
+    Genres()
+}
