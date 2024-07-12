db687e779509b179db4fd6d459363193f91940ca
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-12 12:26:51 +0200

    extract blurry background rectangles to own file/view for better refactoring and usability

--- booklove/Welcome.swift
+++ booklove/Welcome.swift
@@ -11,34 +11,7 @@ struct Welcome: View {
                 .edgesIgnoringSafeArea(.all)
             
             // Background Blur Elements
-            Rectangle()
-                .foregroundColor(.clear)
-                .frame(width: 307.14682, height: 292.85287)
-                .background(
-                    Image("blur_base")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 307.14682, height: 292.85287)
-                        .clipped()
-                )
-                .cornerRadius(65)
-                .shadow(color: .white.opacity(0.25), radius: 3, x: -10, y: 4)
-                .blur(radius: 6)
-                .rotationEffect(Angle(degrees: -58.16))
-                .offset(x: -130, y: -300)
-            
-            Rectangle()
-                .foregroundColor(.clear)
-                .frame(width: 375.42, height: 341.27)
-                .background(
-                    Image("blur_hex")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                )
-                .cornerRadius(65)
-                .offset(x: 130, y: 300)
-                .rotationEffect(.degrees(-6.25))
-                .blur(radius: 6)
+            BackgroundBlurElement()
             
             VStack {
                 Text("booklove.")
