1b2c1090a2b790657f2c6d1e5cb1f1b244f2cbca
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-12 12:51:24 +0200

    create settings app

---
+++ booklove/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  booklove
+//
+//  Created by Moritz on 12.07.24.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
