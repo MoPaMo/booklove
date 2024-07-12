edd4961d8728265b591e16978cf158934d461b7b
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-12 14:15:24 +0200

    keep text black

--- booklove/Settings.swift
+++ booklove/Settings.swift
@@ -22,18 +22,18 @@ struct SettingsView: View {
                     Text("Book Vendor")
                         .font(.system(.title2, design: .serif))
                     Link("Feedback", destination: URL(string: "mailto:feedback@example.com")!)
-                        .font(.system(.title2, design: .serif))
+                        .font(.system(.title2, design: .serif)).foregroundColor(.black)
                     Button(action: {
                         let activityVC = UIActivityViewController(activityItems: ["Check out this awesome app!"], applicationActivities: nil)
                         UIApplication.shared.windows.first?.rootViewController?.present(activityVC, animated: true, completion: nil)
                     }) {
                         Text("Tell A Friend")
-                            .font(.system(.title2, design: .serif))
+                            .font(.system(.title2, design: .serif)).foregroundColor(.black)
                     }
                     Link("Terms Of Service", destination: URL(string: "https://example.com/tos")!)
-                        .font(.system(.title2, design: .serif))
+                        .font(.system(.title2, design: .serif)).foregroundColor(.black)
                     Link("Privacy Policy", destination: URL(string: "https://example.com/privacy")!)
-                        .font(.system(.title2, design: .serif))
+                        .font(.system(.title2, design: .serif)).foregroundColor(.black)
                     Text("Sign Out")
                         .font(.system(.title2, design: .serif))
                         .foregroundColor(.red)
