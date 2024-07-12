bca1fed19eef01362acffcfb3428d04e6ebfec7e
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-12 14:09:54 +0200

    improve font size

--- booklove/Settings.swift
+++ booklove/Settings.swift
@@ -13,58 +13,58 @@ struct SettingsView: View {
             ZStack {
                 BackgroundBlurElement()
                 
-                VStack( spacing: 20) {
+                VStack(spacing: 20) {
                     NavigationLink(destination: AccountView()) {
                         Text("Account")
-                            .font(.system(.title3, design: .serif))
+                            .font(.system(.title2, design: .serif))
                             .foregroundColor(.black)
                     }
                     Text("Book Vendor")
-                        .font(.system(.title3, design: .serif))
+                        .font(.system(.title2, design: .serif))
                     Text("Feedback")
-                        .font(.system(.title3, design: .serif))
+                        .font(.system(.title2, design: .serif))
                     Text("Tell A Friend")
-                        .font(.system(.title3, design: .serif))
+                            .font(.system(.title2, design: .serif))
+                    
                     Text("Terms Of Service")
-                        .font(.system(.title3, design: .serif))
+                        .font(.system(.title2, design: .serif))
                     Text("Privacy Policy")
-                        .font(.system(.title3, design: .serif))
+                        .font(.system(.title2, design: .serif))
                     Text("Sign Out")
-                        .font(.system(.title3, design: .serif))
+                        .font(.system(.title2, design: .serif))
                         .foregroundColor(.red)
                     
                     Spacer()
                 }
-                .padding([.top, .bottom, .trailing]) // Only set padding for top, bottom, and trailing
-                .padding(.leading, 30) // Set leading padding specifically here
+                .padding([.top, .bottom, .trailing])
+                .padding(.leading, 30)
             }
         }
     }
 }
 
-
 struct AccountView: View {
     var body: some View {
-        ZStack{
+        ZStack {
             BackgroundBlurElement()
-            VStack(alignment: .leading, spacing: 20) {
+            VStack(spacing: 20) {
                 
                 Text("Account")
-                    .font(.system(.title3, design: .serif))
+                    .font(.system(.title2, design: .serif))
                     .foregroundColor(.black)
                 
                 Text("Set New Name")
-                    .font(.system(.title3, design: .serif))
+                    .font(.system(.title2, design: .serif))
                 Text("Download My Data")
-                    .font(.system(.title3, design: .serif))
+                    .font(.system(.title2, design: .serif))
                 Text("Delete Account")
-                    .font(.system(.title3, design: .serif))
+                    .font(.system(.title2, design: .serif))
                     .foregroundColor(.red)
                 
                 Spacer()
             }
-            .padding(.leading, 18) // Setzt den linken Rand auf 30 Pixel
-            .padding([.top, .bottom, .trailing]) // Setzt die anderen Ränder auf den Standardwert
+            .padding(.leading, 18)
+            .padding([.top, .bottom, .trailing])
         }
     }
 }
