e2bd75ff0c802dfd971cf45b679c4d02858d0203
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-14 18:02:13 +0200

    add more details to user page

--- booklove/User.swift
+++ booklove/User.swift
@@ -1,54 +1,69 @@
 import SwiftUI
 
-struct UserProfileHeaderView: View {
+struct UserProfileView: View {
     @State var followed = false;
     var body: some View {
-        HStack(spacing: 15) {
-            // Profile Picture
-            Image("memoji_placeholder")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 60, height: 60)
-                .clipShape(Circle())
-                .shadow(radius: 5)
-            
-            VStack(alignment: .leading, spacing: 5) {
-                // User Name
-                Text("Jane Appleseed")
-                    .font(.system(size: 28, weight: .bold, design: .serif))
-                    .foregroundColor(.mint)
-                
-                // Favourite Book
-                Text("\(Image(systemName: "book.circle.fill")) Pride and Prejudice")
-                    .font(.system( size: 18, weight:.regular))
+        ZStack{
+            BackgroundBlurElement()
+            VStack{
+                Text("booklove.")
+                    .font(.system(size: 64, weight: .bold, design: .serif))
                     .foregroundColor(.black)
-            }
-            
-            Spacer()
-            
-            // Follow Button
-            Button(action: {
-                followed=(followed==false)
-            }) {
-                if(!followed){
-                Image(systemName: "heart")
-                    .font(.title2)
-                    .foregroundColor(.red)}
-                else{
-                    Image(systemName: "heart.fill")
-                        .font(.title2)
-                        .foregroundColor(.red)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 20)
+                    .padding(.bottom)
+                
+                HStack(spacing: 15) {
+                    // Profile Picture
+                    Image("memoji_placeholder")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 70, height: 70)
+                        .clipShape(Circle())
+                        .shadow(radius: 5)
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        // User Name
+                        Text("Jane Appleseed")
+                            .font(.system(size: 30, weight: .bold, design: .serif))
+                            .foregroundColor(.mint)
+                        
+                        // Favourite Book
+                        Text("\(Image(systemName: "book.circle.fill")) Pride and Prejudice")
+                            .font(.system( size: 18, weight:.regular))
+                            .foregroundColor(.black)
+                    }
+                    
+                    
+                    Spacer()
+                    
+                    // Follow Button
+                    Button(action: {
+                        followed=(followed==false)
+                    }) {
+                        if(!followed){
+                            Image(systemName: "heart")
+                                .font(.title2)
+                            .foregroundColor(.red)}
+                        else{
+                            Image(systemName: "heart.fill")
+                                .font(.title2)
+                                .foregroundColor(.red)
+                        }
+                    }
                 }
+                .padding()
+                
+                .cornerRadius(10)            
+                Spacer()
+
             }
         }
-        .padding()
-        
-        .cornerRadius(10)
     }
 }
 
 struct UserProfileHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        UserProfileHeaderView()
+        UserProfileView()
     }
 }
