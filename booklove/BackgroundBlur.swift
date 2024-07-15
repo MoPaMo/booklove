84ce6457a182f53e3091124ac25dc102557c7c47
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-15 23:45:33 +0200

    add alternate backgrouond option

--- booklove/BackgroundBlur.swift
+++ booklove/BackgroundBlur.swift
@@ -8,10 +8,27 @@
 import SwiftUI
 
 struct BackgroundBlurElement: View {
+    var option: Int
+    
+    init(option: Int = 1) {
+        self.option = option
+    }
+    
     var body: some View {
         ZStack {
             Color(red: 1, green: 0.98, blue: 0.98)
                 .edgesIgnoringSafeArea(.all)
+            
+            if option == 2 {
+                secondBackground
+            } else {
+                defaultBackground
+            }
+        }
+    }
+    
+    var defaultBackground: some View {
+        ZStack {
             Rectangle()
                 .foregroundColor(.clear)
                 .frame(width: 307.14682, height: 292.85287)
@@ -42,8 +59,43 @@ struct BackgroundBlurElement: View {
                 .blur(radius: 6)
         }
     }
+    
+    var secondBackground: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(width: 350, height: 330)
+                .background(
+                    Image("blur3")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 350, height: 330)
+                        .clipped()
+                )
+                .cornerRadius(50)
+                .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 5)
+                .blur(radius: 4)
+                .rotationEffect(Angle(degrees: 30))
+                .offset(x: 100, y: -250)
+            
+            Circle()
+                .foregroundColor(.clear)
+                .frame(width: 300, height: 300)
+                .background(
+                    Image("blur1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                )
+                .blur(radius: 5)
+                .offset(x: -120, y: 280)
+        }
+    }
 }
 
 #Preview {
     BackgroundBlurElement()
+}
+
+#Preview {
+    BackgroundBlurElement(option: 2)
 }
