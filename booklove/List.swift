c6d8be69fd50d30b0e297e296fb6dc3f2a5640bc
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-18 20:07:34 +0200

    add loading skeleton

--- booklove/List.swift
+++ booklove/List.swift
@@ -5,8 +5,6 @@
 //  Created by Moritz on 11.07.24.
 //
 
-
-
 import SwiftUI
 import Alamofire
 
@@ -23,26 +21,25 @@ struct BookItem: Identifiable {
         }
 }
 
+
 struct List: View {
-    @State private var isSheetPresented = false;
+    @State private var isSheetPresented = false
     @State var books: [BookItem] = []
+    @State private var isLoading = true
     
     var body: some View {
         NavigationView {
             ZStack {
                 // Background Color
-                BackgroundBlurElement(option:2)
+                BackgroundBlurElement(option: 2)
                     .edgesIgnoringSafeArea(.all)
+                
                 ScrollView {
                     VStack(alignment: .leading) {
-                        
-                        
                         Text("Your\nBooks")
                             .font(.system(size: 64, weight: .bold, design: .serif))
                             .foregroundColor(.orange)
-                        
                             .padding(.top, 100)
-                        
                         
                         Text("By Genre")
                             .font(.system(size: 24, weight: .regular, design: .serif))
@@ -64,46 +61,49 @@ struct List: View {
                         }
                         .padding(.bottom, 20)
                         
-                        
-                        
-                        VStack(alignment: .leading, spacing: 10) {
-                            ForEach(books) { book in
-                                VStack(alignment: .leading) {
-                                    NavigationLink(destination: Book()){
-                                        VStack(alignment: .leading) {
-                                            Text(book.title)
-                                                .font(.system(size: 24, weight: .bold, design: .serif))
-                                                .foregroundColor(.cyan)
-                                            
-                                            Text("\(book.author), \(book.year)")
-                                                .font(.system(size: 18, weight: .light, design: .serif))
-                                                .foregroundColor(.black)
+                        if isLoading {
+                            LoadingSkeleton()
+                        } else {
+                            VStack(alignment: .leading, spacing: 10) {
+                                ForEach(books) { book in
+                                    VStack(alignment: .leading) {
+                                        NavigationLink(destination: Book()) {
+                                            VStack(alignment: .leading) {
+                                                Text(book.title)
+                                                    .font(.system(size: 24, weight: .bold, design: .serif))
+                                                    .foregroundColor(.cyan)
+                                                
+                                                Text("\(book.author), \(book.year)")
+                                                    .font(.system(size: 18, weight: .light, design: .serif))
+                                                    .foregroundColor(.black)
+                                            }
                                         }
                                     }
+                                    .padding(.bottom, 10)
                                 }
-                                .padding(.bottom, 10)
                             }
-                            
-                        }.onAppear(perform: fetchBooks)
+                        }
                     }
                 }
                 .padding(.horizontal, 20)
+                
                 VStack {
                     HStack {
                         Spacer()
                         Image(systemName: "magnifyingglass.circle.fill")
                             .foregroundColor(.black)
                             .font(.system(size: 32))
-                            .padding().onTapGesture {
+                            .padding()
+                            .onTapGesture {
                                 isSheetPresented = true
                             }
                     }
                     Spacer()
                 }
-            }.sheet(isPresented: $isSheetPresented) {
+            }
+            .sheet(isPresented: $isSheetPresented) {
                 SearchView()
             }
-            
             .safeAreaInset(edge: .top) {
                 ZStack {
                     Rectangle()
@@ -113,15 +113,16 @@ struct List: View {
                         .frame(height: 0)
                 }
             }
+            .onAppear(perform: fetchBooks)
         }
     }
+    
     func fetchBooks() {
         let headers: HTTPHeaders = [
             "Authorization": "Bearer \(SecureStorage.get() ?? "")",
             "Content-Type": "application/json"
         ]
-        AF.request("https://api.booklove.top/user/list", method:.post, encoding: JSONEncoding.default, headers: headers).responseDecodable(of:Response.self)
-        {response in
+        AF.request("https://api.booklove.top/user/list", method: .post, encoding: JSONEncoding.default, headers: headers).responseDecodable(of: Response.self) { response in
             switch response.result {
             case .success(let responseData):
                 // Access the nested data
@@ -129,11 +130,12 @@ struct List: View {
                     return BookItem(id: k.id, title: k.title,
                                     author: k.author,
                                     year: k.year ?? "sometime")
-                   }
+                }
+                isLoading = false
                 
             case .failure(let error):
-                
                 print("Error: \(error)")
+                isLoading = false
             }
         }
     }
@@ -141,4 +143,24 @@ struct List: View {
 
 #Preview {
     List()
+}
+
+
+struct LoadingSkeleton: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            ForEach(0..<5) { _ in
+                VStack(alignment: .leading, spacing: 5) {
+                    RoundedRectangle(cornerRadius: 5)
+                        .fill(Color.gray.opacity(0.3))
+                        .frame(height: 30)
+                    
+                    RoundedRectangle(cornerRadius: 5)
+                        .fill(Color.gray.opacity(0.3))
+                        .frame(height: 20)
+                }
+                .padding(.vertical, 5)
+            }
+        }
+    }
 }
