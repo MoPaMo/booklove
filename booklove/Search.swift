cedcbd8ebc997fc810bbc0a295977f8cafca72d4
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-18 10:24:29 +0200

    work on better book search

--- booklove/Search.swift
+++ booklove/Search.swift
@@ -15,30 +15,32 @@ struct SearchView: View {
     @EnvironmentObject var tabViewModel: TabViewModel
 
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "magnifyingglass")
-                    .foregroundColor(.gray)
+        ScrollView{
+            VStack {
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.gray)
+                    
+                    TextField("Search for books, authors, or readers", text: $searchText, onCommit: {
+                        fetchBooks()
+                    }).focused($isFocused).onAppear(perform: {
+                        isFocused = true
+                    })
+                    .font(.custom("Georgia", size: 16))
+                    .padding(8)
+                }
+                .padding()
+                .background(BlurView(style: .systemMaterial))
+                .cornerRadius(10)
+                .padding([.leading, .trailing], 16)
+                .padding(.top, 50)
                 
-                TextField("Search for books, authors, or readers", text: $searchText, onCommit: {
-                    fetchBooks()
-                }).focused($isFocused).onAppear(perform: {
-                    isFocused = true
-                })
-                .font(.custom("Georgia", size: 16))
-                .padding(8)
-            }
-            .padding()
-            .background(BlurView(style: .systemMaterial))
-            .cornerRadius(10)
-            .padding([.leading, .trailing], 16)
-            
-            if isLoading {
-                ProgressView()
-                    .progressViewStyle(CircularProgressViewStyle())
-                    .padding()
-            } else {
-                
+                if isLoading {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .padding()
+                } else {
+                    
                     ForEach(searchResults, id: \.self) { result in
                         VStack(alignment: .leading) {
                             Text(result)
@@ -53,67 +55,74 @@ struct SearchView: View {
                         }
                         .padding(.vertical, 8)
                     }
-                
-
+                    
+                    
+                }
             }
+            .background(Color.white.edgesIgnoringSafeArea(.all))
+            .overlay(
+                BackgroundBlur()
+            )
         }
-        .background(Color.white.edgesIgnoringSafeArea(.all))
-        .overlay(
-            BackgroundBlur()
-        )
     }
     
     func fetchBooks() {
-            guard let url = URL(string: "https://api.booklove.top/book/search") else {
-                print("Invalid URL")
-                return
-            }
-            
-            // Prepare the request
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            
-            // Add bearer token from SecureStorage as authorization header
-            if let bearerToken = SecureStorage.get() {
-                request.setValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
-            }
-            
-            // Create the request body
-            let body: [String: Any] = [
-                "query": "your_query_here"
-            ]
-            
-            do {
-                request.httpBody = try JSONSerialization.data(withJSONObject: body)
-            } catch {
-                print("Error serializing JSON: \(error)")
-                return
-            }
-            
-            // Perform the request
-            URLSession.shared.dataTask(with: request) { data, response, error in
-                if let error = error {
-                    print("Error fetching books: \(error.localizedDescription)")
-                    return
-                }
-                
-                guard let data = data else {
-                    print("No data received")
-                    return
-                }
-                
-                // Decode JSON response
-                do {
-                    let decodedResponse = try JSONDecoder().decode(BookResponse.self, from: data)
-                    DispatchQueue.main.async {
-                        self.searchResults = decodedResponse.data.books
-                    }
-                } catch {
-                    print("Error decoding JSON: \(error)")
-                }
-            }.resume()
+        guard let url = URL(string: "https://api.booklove.top/book/search") else {
+            print("Invalid URL")
+            return
         }
+        
+        // Prepare the request
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // Add bearer token from SecureStorage as authorization header
+        if let bearerToken = SecureStorage.get() {
+            request.setValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
+        }
+        
+        // Create the request body
+        let body: [String: Any] = [
+            "query": "your_query_here"
+        ]
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: body)
+        } catch {
+            print("Error serializing JSON: \(error)")
+            return
+        }
+        
+        // Perform the request
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error fetching books: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data received")
+                return
+            }
+            
+            // Decode JSON response
+            do {
+                let decodedResponse = try JSONDecoder().decode(BookResponse.self, from: data)
+                DispatchQueue.main.async {
+                    self.books = decodedResponse.data.books.compactMap { book in
+                        if let author = book.author, let year = book.year {
+                            return BookItem(title: book.title, author: author, year: year)
+                        } else {
+                            return nil
+                        }
+                    }
+                }
+            } catch {
+                print("Error decoding JSON: \(error)")
+            }
+        }.resume()
+    }
     
 
 }
@@ -150,7 +159,14 @@ struct BookResponse: Codable {
 }
 
 struct BookData: Codable {
-    let books: [String]
+    let books: [Book]
+}
+
+struct BookRes: Codable {
+    let title: String
+    let author: String?
+    let id: String
+    let year: Int?
 }
 
 #Preview {
