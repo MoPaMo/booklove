f1d453d7d88140bd57541afedd6809fc692313b3
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-19 18:03:58 +0200

    add acknowledgements

--- booklove/Settings.swift
+++ booklove/Settings.swift
@@ -82,6 +82,11 @@ struct SettingsView: View {
                             .presentationDetents([.medium, .large])
                             .presentationDragIndicator(.hidden)
                     }
+                    NavigationLink(destination: AcknowledgementsView()){
+                        Text("Acknowledgments")
+                            .font(.system(size: 32, weight: .regular, design: .serif))
+                            .foregroundColor(.black)
+                    }
                     Link("Terms Of Service", destination: URL(string: "https://example.com/tos")!)
                         .font(.system(size: 32, weight: .regular, design: .serif))
                         .foregroundColor(.black)
@@ -122,6 +127,50 @@ struct AccountView: View {
                 Text("Delete Account")
                     .font(.system(size: 32, weight: .regular, design: .serif))
                     .foregroundColor(.red)
+                
+                Spacer()
+            }
+            .padding(.leading, 18)
+            .padding([.top, .bottom, .trailing])
+        }
+    }
+}
+
+struct AcknowledgementsView: View {
+    var body: some View {
+        ZStack {
+            BackgroundBlurElement().edgesIgnoringSafeArea(.all)
+            VStack(spacing: 50) {
+               
+                Text("""
+                    This Project uses data from the OpenLibrary API.
+                     
+                    
+                    This Project uses Alamofire
+                    
+                     Copyright (c) 2014-2022 Alamofire Software Foundation (http://alamofire.org/)
+
+                     Permission is hereby granted, free of charge, to any person obtaining a copy
+                     of this software and associated documentation files (the "Software"), to deal
+                     in the Software without restriction, including without limitation the rights
+                     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                     copies of the Software, and to permit persons to whom the Software is
+                     furnished to do so, subject to the following conditions:
+
+                     The above copyright notice and this permission notice shall be included in
+                     all copies or substantial portions of the Software.
+
+                     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+                     THE SOFTWARE.
+                    """)
+                    .font(.system(size: 16, weight: .regular, design: .monospaced))
+                    .foregroundColor(.black)
+               
                 
                 Spacer()
             }
